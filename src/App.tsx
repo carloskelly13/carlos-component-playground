a68c596cb604cac71a9b6ae7dbba13a8906d2e01
Carlos Kelly <me@carlos.dev>
2023-12-29 16:49:12 -0600

    Add Dialog Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from '@/components/ui/dialog'
 
 function App() {
   return (
@@ -21,6 +30,60 @@ function App() {
           <Button intent="destructive" size="medium">
             Destructive, Medium
           </Button>
+        </div>
+        <div>
+          <h1 className="text-black dark:text-white text-xl font-bold mt-12 mb-2">
+            Dialogs
+          </h1>
+          {/* A Primary Dialog */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button intent="primary" size="small">
+                Start Test
+              </Button>
+            </DialogTrigger>
+            <DialogContent showClose>
+              <DialogHeader>
+                <DialogTitle>Start Load Test</DialogTitle>
+                <DialogDescription>
+                  Paste in the code you wish to test.
+                </DialogDescription>
+              </DialogHeader>
+              <textarea className="w-full h-32 p-4 font-mono text-xs" />
+              <div className="flex justify-end">
+                <DialogClose asChild>
+                  <Button size="small">Start Test</Button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
+          {/* A Destructive Dialog */}
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="ml-4" intent="destructive" size="small">
+                Self-Destruct
+              </Button>
+            </DialogTrigger>
+            {/* 👇 Example of a custom intent (through a CVA prop) */}
+            <DialogContent intent="destructive">
+              <DialogHeader>
+                <DialogTitle>Self-Destruct</DialogTitle>
+                <DialogDescription>
+                  Are you sure you want to blow everything up?
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex justify-end [&>button]:ml-4">
+                <DialogClose asChild>
+                  <Button size="small" intent="destructive">
+                    Blow Everything Up
+                  </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button size="small">Cancel</Button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </>
